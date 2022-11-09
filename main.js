a85a1894c63ac3d731398ce2f47578fdd16d67bd
Ziad Aboalmjd <ziadgamalaboalmajd@gmail.com>
2022-11-09 21:59:40 +0200

    last

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
+// @ input dom
+const titleCont = document.getElementById("titleCont");
+const sendtoCont = document.getElementById("sendToCont");
+const messCont = document.getElementById("messCont");
+const greetC = document.getElementById("greetC");
+const closingC = document.getElementById("closingC");
+const stampC = document.getElementById("stampC");
+const amlC = document.getElementById("amlC");
+const ahmC = document.getElementById("ahmC");
+// @ output dom
+const titleFv = document.querySelector(".titleContf");
+const sendtoFv = document.querySelector(".sendToContf");
+const messFv = document.querySelector(".messContf");
+const greetFv = document.querySelector(".greetCont");
+const closingFv = document.querySelector(".closingCont");
+const stampFv = document.querySelector(".stampC");
+const amlFv = document.querySelector(".amlC");
+const ahmFv = document.querySelector(".ahmC");
+// @ btn
+const printbtn = document.getElementById("generate");
+
+function typeData(){
+    // retrive values
+    let titleValue = titleCont.textContent
+    let sendtoValue = sendtoCont.textContent
+    let messValue = messCont.textContent
+    let greetValue = greetC.checked
+    let closingValue = closingC.checked
+    let stampValue = stampC.checked
+    let amlCValue = amlC.checked
+    let ahmCValue = ahmC.checked
+    // use the values
+    titleFv.textContent = titleValue
+    sendtoFv.textContent = sendtoValue
+    messFv.textContent = messValue
+    greetValue ? greetFv.style.display = "block" : greetFv.style.display = "none"
+    closingValue ? closingFv.style.display = "block" : closingFv.style.display = "none"
+    stampValue ? stampFv.src = "img/001__2_-removebg-preview.png" : stampFv.src = ""
+    amlCValue ? amlFv.src = "img/امضاء__2_-removebg-preview.png" : amlFv.src = ""
+    ahmCValue ? ahmFv.src = "img/photo_2022-10-27_01-50-10-removebg-preview.png" : ahmFv.src = ""
+}
+
+printbtn.addEventListener("click", function(){
+    typeData()
+    window.print();
+});
 // function PrintElem(elem)
 // {
 //     var mywindow = window.open('', 'PRINT', 'height=400,width=600');
@@ -16,9 +62,3 @@
 
 //     return true;
 // }
-
-const printbtn = document.getElementById("generate");
-
-printbtn.addEventListener("click", function(){
-    window.print();
-});
