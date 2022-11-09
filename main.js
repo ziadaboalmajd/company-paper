c876c78c7a26339d763387fe4d1cd468ad28017f
Ziad Aboalmjd <ziadgamalaboalmajd@gmail.com>
2022-11-09 23:15:29 +0200

    initial

--- main.js
+++ main.js
@@ -1,342 +1,63 @@
 // @ input dom
 const titleCont = document.getElementById("titleCont");
-// function PrintElem(elem)
-// {
-//     var mywindow = window.open('', 'PRINT', 'height=400,width=600');
-
-//     mywindow.document.write('<html><head><title>' + document.title  + '</title>');
-//     mywindow.document.write('</head><body >');
-//     mywindow.document.write('<h1>' + document.title  + '</h1>');
-//     mywindow.document.write(document.getElementById(elem).innerHTML);
-//     mywindow.document.write('</body></html>');
-
-//     mywindow.document.close(); // necessary for IE >= 10
-//     mywindow.focus(); // necessary for IE >= 10*/
-
-//     mywindow.print();
-//     mywindow.close();d
-
-//     return true;
-// }
-const sendtoCont = document.getElementById("sendToCont");    //     mywindow.document.close(); // necessary for IE >= 10
-//     mywindow.focus(); // necessary for IE >= 10*/
-
-//     mywindow.print();
-//     mywindow.close();
-
-//     return true;
-// }
+const sendtoCont = document.getElementById("sendToCont");
 const messCont = document.getElementById("messCont");
 const greetC = document.getElementById("greetC");
-const closingC = document.getElementById("closingC");// function PrintElem(elem)
-// {
-//     var mywindow = window.open('', 'PRINT', 'height=400,width=600');
-
-//     mywindow.document.write('<html><head><title>' + document.title  + '</title>');
-//     mywindow.document.write('</head><body >');
-//     mywindow.document.write('<h1>' + document.title  + '</h1>');
-//     mywindow.document.write(document.getElementById(elem).innerHTML);
-//     mywindow.document.write('</body></html>');
-
-//     mywindow.document.close(); // necessary for IE >= 10
-//     mywindow.focus(); // necessary for IE >= 10*/
-
-//     mywindow.print();
-//     mywindow.close();
-
-//     return true;
-// }
-const stampC = document.getElementById("stampC");    //     mywindow.document.close(); // necessary for IE >= 10
-//     mywindow.focus(); // necessary for IE >= 10*/
-
-//     mywindow.print();
-//     mywindow.close();
-
-//     return true;
-// }
-const amlC = document.getElementById("amlC");    //     mywindow.document.close(); // necessary for IE >= 10
-//     mywindow.focus(); // necessary for IE >= 10*/
-
-//     mywindow.print();
-//     mywindow.close();
-
-//     return true;
-// }
-const ahmC = document.getElementById("ahmC");    //     mywindow.document.close(); // necessary for IE >= 10
-//     mywindow.focus(); // necessary for IE >= 10*/
-
-//     mywindow.print();
-//     mywindow.close();
-
-//     return true;
-// }
+const closingC = document.getElementById("closingC");
+const stampC = document.getElementById("stampC");
+const amlC = document.getElementById("amlC");
+const ahmC = document.getElementById("ahmC");
 // @ output dom
-const titleFv = document.querySelector(".titleContf");// function PrintElem(elem)
-// {
-//     var mywindow = window.open('', 'PRINT', 'height=400,width=600');
-
-//     mywindow.document.write('<html><head><title>' + document.title  + '</title>');
-//     mywindow.document.write('</head><body >');
-//     mywindow.document.write('<h1>' + document.title  + '</h1>');
-//     mywindow.document.write(document.getElementById(elem).innerHTML);
-//     mywindow.document.write('</body></html>');
-
-//     mywindow.document.close(); // necessary for IE >= 10
-//     mywindow.focus(); // necessary for IE >= 10*/
-
-//     mywindow.print();
-//     mywindow.close();
-
-//     return true;
-// }
-const sendtoFv = document.querySelector(".sendToContf");    //     mywindow.document.close(); // necessary for IE >= 10
-//     mywindow.focus(); // necessary for IE >= 10*/
-
-//     mywindow.print();
-//     mywindow.close();
-
-//     return true;
-// }
-const messFv = document.querySelector(".messContf");    //     mywindow.document.close(); // necessary for IE >= 10
-//     mywindow.focus(); // necessary for IE >= 10*/
-
-//     mywindow.print();
-//     mywindow.close();
-
-//     return true;
-// }
+const titleFv = document.querySelector(".titleContf");
+const sendtoFv = document.querySelector(".sendToContf");
+const messFv = document.querySelector(".messContf");
 const greetFv = document.querySelector(".greetCont");
 const closingFv = document.querySelector(".closingCont");
-const stampFv = document.querySelector(".stampC");// function PrintElem(elem)
-// {
-//     var mywindow = window.open('', 'PRINT', 'height=400,width=600');
-
-//     mywindow.document.write('<html><head><title>' + document.title  + '</title>');
-//     mywindow.document.write('</head><body >');
-//     mywindow.document.write('<h1>' + document.title  + '</h1>');
-//     mywindow.document.write(document.getElementById(elem).innerHTML);
-//     mywindow.document.write('</body></html>');
-
-//     mywindow.document.close(); // necessary for IE >= 10
-//     mywindow.focus(); // necessary for IE >= 10*/
-
-//     mywindow.print();
-//     mywindow.close();
-
-//     return true;
-// }
+const stampFv = document.querySelector(".stampC");
 const amlFv = document.querySelector(".amlC");
 const ahmFv = document.querySelector(".ahmC");
 // @ btn
 const printbtn = document.getElementById("generate");
-const passBtn = document.getElementById("passBtn");// function PrintElem(elem)
-// {
-//     var mywindow = window.open('', 'PRINT', 'height=400,width=600');
-
-//     mywindow.document.write('<html><head><title>' + document.title  + '</title>');
-//     mywindow.document.write('</head><body >');
-//     mywindow.document.write('<h1>' + document.title  + '</h1>');
-//     mywindow.document.write(document.getElementById(elem).innerHTML);
-//     mywindow.document.write('</body></html>');
-
-//     mywindow.document.close(); // necessary for IE >= 10
-//     mywindow.focus(); // necessary for IE >= 10*/
-
-//     mywindow.print();
-//     mywindow.close();
-
-//     return true;
-// }
+const passBtn = document.getElementById("passBtn");
 // @ secuirty
-const weakPass = document.getElementById("weakPass");    //     mywindow.document.close(); // necessary for IE >= 10
-//     mywindow.focus(); // necessary for IE >= 10*/
-
-//     mywindow.print();
-//     mywindow.close();
-
-//     return true;
-// }
-const weakQ = document.querySelector(".weakQ");    //     mywindow.document.close(); // necessary for IE >= 10
-//     mywindow.focus(); // necessary for IE >= 10*/
-
-//     mywindow.print();
-//     mywindow.close();
-
-//     return true;
-// }
+const weakPass = document.getElementById("weakPass");
+const weakQ = document.querySelector(".weakQ");
 const passValue = document.getElementById("passValue");
 let admin = false
 
 function typeData() {
     // retrive values
-    let titleValue = titleCont.textContent// function PrintElem(elem)
-    // {
-    //     var mywindow = window.open('', 'PRINT', 'height=400,width=600');
-    
-    //     mywindow.document.write('<html><head><title>' + document.title  + '</title>');
-    //     mywindow.document.write('</head><body >');
-    //     mywindow.document.write('<h1>' + document.title  + '</h1>');
-    //     mywindow.document.write(document.getElementById(elem).innerHTML);
-    //     mywindow.document.write('</body></html>');
-    
-    //     mywindow.document.close(); // necessary for IE >= 10
-    //     mywindow.focus(); // necessary for IE >= 10*/
-    
-    //     mywindow.print();
-    //     mywindow.close();
-    
-    //     return true;
-    // }
+    let titleValue = titleCont.textContent
     let sendtoValue = sendtoCont.textContent
-    let messValue = messCont.textContent// function PrintElem(elem)
-    // {
-    //     var mywindow = window.open('', 'PRINT', 'height=400,width=600');
-    
-    //     mywindow.document.write('<html><head><title>' + document.title  + '</title>');
-    //     mywindow.document.write('</head><body >');
-    //     mywindow.document.write('<h1>' + document.title  + '</h1>');
-    //     mywindow.document.write(document.getElementById(elem).innerHTML);
-    //     mywindow.document.write('</body></html>');
-    
-    //     mywindow.document.close(); // necessary for IE >= 10
-    //     mywindow.focus(); // necessary for IE >= 10*/
-    
-    //     mywindow.print();
-    //     mywindow.close();
-    
-    //     return true;
-    // }
+    let messValue = messCont.textContent
     let greetValue = greetC.checked
-    let closingValue = closingC.checked// function PrintElem(elem)
-    // {
-    //     var mywindow = window.open('', 'PRINT', 'height=400,width=600');
-    
-    //     mywindow.document.write('<html><head><title>' + document.title  + '</title>');
-    //     mywindow.document.write('</head><body >');
-    //     mywindow.document.write('<h1>' + document.title  + '</h1>');
-    //     mywindow.document.write(document.getElementById(elem).innerHTML);
-    //     mywindow.document.write('</body></html>');
-    
-    //     mywindow.document.close(); // necessary for IE >= 10
-    //     mywindow.focus(); // necessary for IE >= 10*/
-    
-    //     mywindow.print();
-    //     mywindow.close();
-    
-    //     return true;
-    // }
+    let closingValue = closingC.checked
     let stampValue = stampC.checked
     let amlCValue = amlC.checked
     let ahmCValue = ahmC.checked
     // use the values
-    titleFv.textContent = titleValue// function PrintElem(elem)
-    // {
-    //     var mywindow = window.open('', 'PRINT', 'height=400,width=600');
-    
-    ow.document.write('<h1>' + document.title  + '</h1>');
-    //     mywindow.document.write(document.getElementById(elem).innerHTML);
-    //     mywindow.document.write('</body></html>');
-    
-    //     mywindow.document.close(); // necessary for IE >= 10
-    //     mywindow.focus(); // necessary for IE >= 10*/
-    
-    //     mywindow.print();
-    //     mywindow.close();
-    
-    //     return true;
-    // }
+    titleFv.textContent = titleValue
     sendtoFv.textContent = sendtoValue
     messFv.textContent = messValue
-    greetValue ? greetFv.style.display = "block" : greetFv.style.display = "none"    //     mywindow.document.close(); // necessary for IE >= 10
-    //     mywindow.focus(); // necessary for IE >= 10*/
-    
-    //     mywindow.print();
-    //     mywindow.close();
-    
-    //     return true;
-    // }    //     mywindow.document.close(); // necessary for IE >= 10
-    //     mywindow.focus(); // necessary for IE >= 10*/
-    
-    //     mywindow.print();
-    //     mywindow.close();
-    
-    //     return true;
-    // }
+    greetValue ? greetFv.style.display = "block" : greetFv.style.display = "none"
     closingValue ? closingFv.style.display = "block" : closingFv.style.display = "none"
     stampValue ? stampFv.src = "img/001__2_-removebg-preview.png" : stampFv.src = ""
-        //     mywindow.document.close(); // necessary for IE >= 10
-    //     mywindow.focus(); // necessary for IE >= 10*/
-    
-    //     mywindow.print();
-    //     mywindow.close();
-    
-    //     return true;
-    // }    //     mywindow.document.close(); // necessary for IE >= 10
-    //     mywindow.focus(); // necessary for IE >= 10*/
-    
-    //     mywindow.print();
-    //     mywindow.close();
-    
-    //     return true;
-    // }
-    amlCValue ? amlFv.src = "img/امضاء__2_-removebg-preview.png" : amlFv.src = ""    //     mywindow.document.close(); // necessary for IE >= 10
-    //     mywindow.focus(); // necessary for IE >= 10*/
-    
-    //     mywindow.print();
-    //     mywindow.close();
-    
-    //     return true;
-    // }
+    amlCValue ? amlFv.src = "img/امضاء__2_-removebg-preview.png" : amlFv.src = ""
     ahmCValue ? ahmFv.src = "img/photo_2022-10-27_01-50-10-removebg-preview.png" : ahmFv.src = ""
 }
 
 printbtn.addEventListener("click", function () {
     if (admin == true) {
-        typeData()    //     mywindow.document.close(); // necessary for IE >= 10
-        //     mywindow.focus(); // necessary for IE >= 10*/
-        
-        //     mywindow.print();
-        //     mywindow.close();
-        
-        //     return true;
-        // }
-        window.print();    //     mywindow.document.close(); // necessary for IE >= 10
-        //     mywindow.focus(); // necessary for IE >= 10*/
-        
-        //     mywindow.print();
-        //     mywindow.close();
-        
-        //     return true;
-        // }
+        typeData()
+        window.print();
     }
 });
 
 passBtn.addEventListener("click", function () {
-    if (passValue.value === "Amola") {    //     mywindow.document.close(); // necessary for IE >= 10
-        //     mywindow.focus(); // necessary for IE >= 10*/
-        
-        //     mywindow.print();
-        //     mywindow.close();
-        
-        //     return true;
-        // }
-        weakPass.style.display = "none"    //     mywindow.document.close(); // necessary for IE >= 10
-        //     mywindow.focus(); // necessary for IE >= 10*/
-        
-        //     mywindow.print();
-        //     mywindow.close();
-        
-        //     return true;
-        // }
+    if (passValue.value === "Amola") {
+        weakPass.style.display = "none"
         weakQ.style.display = "none"
-        admin = true    //     mywindow.document.close(); // necessary for IE >= 10
-        //     mywindow.focus(); // necessary for IE >= 10*/
-        
-        //     mywindow.print();
-        //     mywindow.close();
-        
-        //     return true;
-        // }
+        admin = true
     }
 });
 
